8ddc82cc2fea1451aa003ec81a620e3762feb2ad
mmr116 <mustafiz.rahman116@gmail.com>
2024-01-27 04:57:41 +0000

    Add .gitignore, main.tf, and terraform.tfvars.template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mongodb-atlas-project and related resources using Terraform
+# mongodb-atlas-project and related resources using Terraformi
+#Update your README file to instruct users to copy terraform.tfvars.template, fill in their actual credentials, and rename it to terraform.tfvars. Make it clear that th#ey should never commit their actual terraform.tfvars to the repository.
---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "1.13.0"
+    }
+  }
+}
+
+provider "mongodbatlas" {
+  public_key  = var.mongodb_atlas_public_key
+  private_key = var.mongodb_atlas_private_key
+}
+
+variable "mongodb_atlas_public_key" {
+  description = "MongoDB Atlas public API key"
+}
+
+variable "mongodb_atlas_private_key" {
+  description = "MongoDB Atlas private API key"
+}
+
+variable "mongodb_atlas_org_id" {
+  description = "MongoDB Atlas organization ID"
+}
+
+resource "mongodbatlas_project" "example" {
+  org_id  = var.mongodb_atlas_org_id
+  name    = var.mongodb_atlas_project_name
+}
+
+variable "mongodb_atlas_project_name" {
+  description = "Name of the MongoDB Atlas project"
+}
