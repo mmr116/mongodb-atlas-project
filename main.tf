400911cabf7f24de5cf3bd93ce84db35c6d58d70
mmr116 <mustafiz.rahman116@gmail.com>
2024-01-27 20:35:41 +0000

    Add GitHub Actions workflow for Terraform120

--- main.tf
+++ main.tf
@@ -8,27 +8,27 @@ terraform {
 }
 
 provider "mongodbatlas" {
-  public_key  = var.TF_VAR_mongodb_atlas_public_key
-  private_key = var.TF_VAR_mongodb_atlas_private_key
+  public_key  = var.mongodb_atlas_public_key
+  private_key = var.mongodb_atlas_private_key
 }
 
-variable "TF_VAR_mongodb_atlas_public_key" {
+variable "mongodb_atlas_public_key" {
   description = "MongoDB Atlas public API key"
 }
 
-variable "TF_VAR_mongodb_atlas_private_key" {
+variable "mongodb_atlas_private_key" {
   description = "MongoDB Atlas private API key"
 }
 
-variable "TF_VAR_mongodb_atlas_org_id" {
+variable "mongodb_atlas_org_id" {
   description = "MongoDB Atlas organization ID"
 }
 
 resource "mongodbatlas_project" "example" {
-  org_id  = var.TF_VAR_mongodb_atlas_org_id
-  name    = var.TF_VAR_mongodb_atlas_project_name
+  org_id  = var.mongodb_atlas_org_id
+  name    = var.mongodb_atlas_project_name
 }
 
-variable "TF_VAR_mongodb_atlas_project_name" {
+variable "mongodb_atlas_project_name" {
   description = "Name of the MongoDB Atlas project"
 }
